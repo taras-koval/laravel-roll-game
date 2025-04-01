26b5c50f2656c7d268309965d6115b4b6cda259e
taras-koval <tkoval37@gmail.com>
2025-04-02 00:14:30 +0200

    Readme documentation, prepare to push and re-install project

--- database/migrations/2025_04_01_142001_create_rolls_table.php
+++ database/migrations/2025_04_01_142001_create_rolls_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('lucky_results');
+        Schema::dropIfExists('rolls');
     }
 };
